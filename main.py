5d410663269ed9570041c21927892548fc7dddab
vr <victor.rij@hotmail.com>
2020-06-12 02:54:58 +0200

    detect similar pixels 2d #2

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class imageComparator(object):
     
   def __init__(self, imageData1, imageData2, threshold, blockResolution, maxColorDiff):
 
-    self.imageData1 = imageData1
+    self.imageData1 = cv.resize(imageData1,(blockResolution,blockResolution)) # Pattern
     self.imageData2 = imageData2
 
     self.maxColorDiff = maxColorDiff
@@ -68,7 +68,7 @@ param_blockResolution = 8
 
 fullLink = './images/mediumSize.jpg'
 templateLink = './images/mediumPattern.jpg'
-param_blockResolution = 16
+param_blockResolution = 8
 
 fullImageSource = cv.imread(fullLink)
 full = imageProcessor(fullLink,0,param_blockResolution)
@@ -89,14 +89,23 @@ while i < nbrLines:
     while z < nbrColumn:
         testBlock = full.getSpecificBlockData(i,z,True)
         tempImageComparator = imageComparator(patternBlock, testBlock[0], 0.7, 8, 3)
+        getTwoBlocksSimilaritiesWithPattern = tempImageComparator.getTwoBlocksSimilarities()
         imageThreshold = tempImageComparator.getTwoBlocksSimilarities()[1]
+        imageThresholdBool = tempImageComparator.getTwoBlocksSimilarities()[0]
         tempRedVal = 255*(1-imageThreshold)
         tempBlueVal = 255*(imageThreshold)
-        cv.rectangle(thresholdMap,(testBlock[1][0][0],testBlock[1][0][1]),(testBlock[1][1][0],testBlock[1][1][1]),(tempBlueVal,tempBlueVal,tempBlueVal),-1)
+        thresholdColor = (tempBlueVal,tempBlueVal,tempBlueVal)
+        if(imageThresholdBool):
+            thresholdColor = (0,255,0)
+        cv.rectangle(thresholdMap,(testBlock[1][0][0],testBlock[1][0][1]),(testBlock[1][1][0],testBlock[1][1][1]),thresholdColor,-1)
         z = z+1
     i = i+1
 
 fullWithThreshold = cv.addWeighted(fullImageSource, 0.25, thresholdMap, 0.75, 1)
 #fullWithThresholdZoom = cv.resize(fullWithThreshold,(500,500), 0, 0, 0, 0)
+filePath = "./gifRes/res_"
+filePath += str(param_blockResolution)
+filePath += "_.jpg"
+cv.imwrite( filePath, fullWithThreshold);
 cv.imshow("1",fullWithThreshold)
 cv.waitKey()
